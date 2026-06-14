131b64b892058ee6a158e703583fb57aca846b49
vs02354-cloud <vs02354@gmail.com>
2026-06-14 23:02:51 +0530

    fix: correctly map paginated driver response in vehicle modal

--- src/app/transport-vehicles/transport-vehicles.ts
+++ src/app/transport-vehicles/transport-vehicles.ts
@@ -38,8 +38,10 @@ export class TransportVehicles implements OnInit {
 
   loadRoutesAndDrivers() {
     this.transportService.getRoutes().subscribe(res => this.availableRoutes = res);
-    this.http.get<any[]>('https://erpschoolapi.onrender.com/api/HR/Employees').subscribe(res => {
-      this.availableDrivers = res.filter(e => e.department === 'Transport' || e.designation === 'Driver');
+    this.http.get<any>('https://erpschoolapi.onrender.com/api/HR/Employees?searchTerm=Driver&pageSize=100').subscribe(res => {
+      if (res && res.items) {
+        this.availableDrivers = res.items.map((item: any) => item.employee);
+      }
     });
   }
 
